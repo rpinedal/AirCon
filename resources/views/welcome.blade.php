2e1e2a2cbb824060bc79243bc2d4556b39f7baa2
Ricardo Lockmer <rpined344@hotmail.com>
2021-01-08 07:57:35 -0600

    start project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        
+    </head>
+    <body class="antialiased bg-indigo-900">
+
+        <div class="h-auto flex border-b-4 text-white justify-center">
+
+            <div class="">
+                <div class="">
+                My Company Logo
+                </div>
+            </div>
+            
+            <div class=" ">
+                <div class="">
+                My Company Logo
+                </div>
+            </div>
+        </div>
+        <div class="h-auto flex border-b-4 text-white justify-center">
+
+            <div class="">
+                <div class="">
+                My Company Logo
+                </div>
+            </div>
+            
+            <div class=" ">
+                <div class="">
+                My Company Logo
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
