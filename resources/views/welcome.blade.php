0074f106b9c4f2483f9cc4a47c76dbd263426e91
Ricardo Lockmer <rpined344@hotmail.com>
2021-01-20 16:52:28 -0600

    first commit
    starting upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,42 +8,233 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
         <!-- Styles -->
         <meta charset="UTF-8" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        
+        <link rel="stylesheet" href="/css/mycss.css">
     </head>
-    <body class="antialiased bg-indigo-900">
+    <body class="antialiased bg-white">
+        <div class="h-4 bg-indigo-900">
 
-        <div class="h-auto flex border-b-4 text-white justify-center">
+        </div>
+        <div class="h-auto mt-10 flex font-bold  justify-center">
 
             <div class="">
-                <div class="">
-                My Company Logo
-                </div>
+                <img data-aos="flip-left"  class="rounded-full h-20 w-44" src="/images/logoAir.png" alt="My Air Company" srcset="">
+                
+                
             </div>
             
-            <div class=" ">
-                <div class="">
-                My Company Logo
+           
+        </div>
+        <div class="h-auto mt-4 font-bold flex text-gray-700 justify-center">
+
+            <div class="mx-4">
+                <a href="###" class="">
+                Services
+                </a>
+            </div>
+            
+            <div class="mx-4">
+                <a href="###" class="">
+               About Us
+                </a>
+            </div>
+            <div class="mx-4">
+                <a href="###" class="">
+               Contact Us
+                </a>
+            </div>
+        </div>
+        <div class=" mt-10 py-4 ">
+                <img class=" h-1/2 w-full" src="/images/lvfam.jpg" alt="" srcset="">
+        </div>
+        <div class="grid grid-cols-6 mt-10 py-4 text-gray-800 justify-center items-center gap-8">
+                <div data-aos="zoom-in-down" data-aos-duration="2000" class="col-start-2 col-end-4 py-4">
+                    <div class="text-justify col-span-11">
+                        <p class="py-4">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi vero fugit numquam eaque voluptates voluptatibus placeat repellendus expedita quae at! Deserunt sint, neque autem vero tenetur pariatur quidem distinctio vel.
+
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem repellendus molestiae aperiam eaque aliquam cum autem ut ab, hic sapiente doloribus iusto quae quam voluptas. Necessitatibus minus nisi maiores facilis.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-start-4 col-end-6 py-4" data-aos="fade-left" data-aos-duration="2000" >
+                    <img class="rounded-3xl" src="/images/ac1.jpg" alt="">
+                </div>
+        </div>
+        <div class="grid grid-cols-6 mt-10 py-4  justify-center items-center gap-8">
+                <div class="col-start-2 col-end-4 py-4">
+                    <div class=" col-span-11">
+                    <img class="rounded-3xl" src="/images/ac2.jpg" data-aos="fade-right" data-aos-duration="2000"  alt="">
+                    </div>
+                </div>
+                <div class="col-start-4 col-end-6 py-4 text-gray-800">
+                    <div data-aos="fade-left" data-aos-duration="2000" class="text-justify col-span-11">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi vero fugit numquam eaque voluptates voluptatibus placeat repellendus expedita quae at! Deserunt sint, neque autem vero tenetur pariatur quidem distinctio vel.
+                    </div>
+                    
+                </div>
+        </div>
+        <div class="mt-10 py-4 ">
+                <div class=" py-4" style="background-image: url('/images/ac3.jpg'); background-repeat: no-repeat;  background-size: cover; background-position: center">
+                    <div data-aos="zoom-in" data-aos-duration="2000" class="grid w-full h-96 object-cover" >
+                    <!-- <img class=" w-full h-96 object-cover" src="/images/ac3.jpg" data-aos="zoom-in" data-aos-duration="2000"  alt=""> -->
+                    <p class="justify-self-center font-bold text-3xl py-4 self-end">Stay Indoors</p>
+                    <p class="w-96 justify-self-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sed corrupti rerum hic molestias, sunt labore? Laboriosam sequi fugiat vitae, minus libero, similique repudiandae quod non rem facere magnam animi?</p>
+                    <div class="flex justify-center">
+                    <a href="###" class="justify-self-center px-2">
+                    <div class="grid justify-self-center mx-2 h-14 w-60 border-4 border-indigo-900 items-center rounded-full">
+                        <div class="justify-self-center font-bold">
+                                Call  Us
+                        </div>
+                        
+                    </div>
+                    </a>
+                    <a href="###" class="justify-self-center px-2">
+                    <div class="grid justify-self-center mx-2 h-14 w-60 border-4 border-indigo-900 items-center rounded-full">
+                        <div class="justify-self-center font-bold">
+                                Contact Us
+                        </div>
+                    </div>  
+                    </a>
+                    </div>
+                    </div>
+                        
+                </div>
+                
+        </div>
+        <div class="grid font-bold justify-center mt-10 ">
+            <h4 class="text-3xl">Our Services</h4>
+        </div>
+        <div data-aos="zoom-in" data-aos-duration="2000" class="h-auto mt-10 flex font-bold  my-4 justify-center ">
+            <div class="grid w-96 px-4 mt-4">
+                <div class="h-20 w-20 border-4 rounded-full p-4 justify-self-center">
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path fill="#fff" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
+                    </svg>
+                </div>
+                <p class="text-justify mt-4 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, molestiae! Vel nesciunt inventore a? Esse dolorum architecto perspiciatis illo corrupti?</p>
+
+            </div> 
+            <div class="grid w-96 px-4 mt-4">
+                <div class="h-20 w-20 border-4 rounded-full p-4 justify-self-center">
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path fill="#fff" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
+                    </svg>
+                </div>
+                <p class="text-justify mt-4 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, molestiae! Vel nesciunt inventore a? Esse dolorum architecto perspiciatis illo corrupti?</p>
+
+            </div>
+            <div class="grid w-96 px-4 mt-4">
+                <div class="h-20 w-20 border-4 rounded-full p-4 justify-self-center">
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path fill="#fff" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
+                    </svg>
+                </div>
+                <p class="text-justify mt-4 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, molestiae! Vel nesciunt inventore a? Esse dolorum architecto perspiciatis illo corrupti?</p>
+
+            </div>   
+        </div>
+        <div data-aos="zoom-in" data-aos-duration="2000" class="h-auto mt-10 flex font-bold  my-4 justify-center ">
+            <div class="grid w-96 px-4 mt-4">
+                <div class="h-20 w-20 border-4 rounded-full p-4 justify-self-center">
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path fill="#fff" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
+                    </svg>
+                </div>
+                <p class="text-justify mt-4 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, molestiae! Vel nesciunt inventore a? Esse dolorum architecto perspiciatis illo corrupti?</p>
+
+            </div> 
+            <div class="grid w-96 px-4 mt-4">
+                <div class="h-20 w-20 border-4 rounded-full p-4 justify-self-center">
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path fill="#fff" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
+                    </svg>
+                </div>
+                <p class="text-justify mt-4 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, molestiae! Vel nesciunt inventore a? Esse dolorum architecto perspiciatis illo corrupti?</p>
+
+            </div>
+            <div class="grid w-96 px-4 mt-4">
+                <div class="h-20 w-20 border-4 rounded-full p-4 justify-self-center">
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path fill="#fff" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" />
+                    </svg>
+                </div>
+                <p class="text-justify mt-4 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est, molestiae! Vel nesciunt inventore a? Esse dolorum architecto perspiciatis illo corrupti?</p>
+
+            </div>   
+        </div>
+        <div data-aos="zoom-in" data-aos-duration="2000" class="grid h-auto  my-32 block font-bold  my-4 justify-center ">
+            <a href="###" class="justify-self-center ">
+            <div class="grid h-14 w-60 border-4 border-indigo-900 items-center rounded-full">
+                <div class="justify-self-center">
+                        CALL  US
+                </div>
+            </div> 
+            </a>
+                <p class="justify-self-center font-light text-gray-700 py-2">Get a free quote on your call!</p>
+        </div>       
+        <div class="grid grid-cols-7 bg-gray-700 py-10 text-white">
+            <div class=" col-start-2">
+                <div class="mx-4 my-2">
+                    <a href="###" class="">
+                    Services
+                    </a>
+                </div>
+                
+                <div class="mx-4 my-2">
+                    <a href="###" class="">
+                About Us
+                    </a>
+                </div>
+                <div class="mx-4 my-2">
+                    <a href="###" class="">
+                Contact Us
+                    </a>
+                </div>
+            </div>
+            <div class=" col-start-4">
+                <img data-aos="flip-left"  class="rounded-full h-20 w-44" src="/images/logoAir.png" alt="My Air Company" srcset="">
+            </div>
+            <div class=" col-start-6 col-end-8">
+                <div class="mx-4 my-2">
+                    <a href="###" class="">
+                        service@aircon.com
+                    </a>
+                </div>
+                <div class="mx-4 my-2">
+                    <a href="###" class="">
+                        +1 5555-5555
+                    </a>
+                </div>
+                <div class="mx-4 my-2">
+                    <a href="###" class="">
+                        Your Address goes here.
+                    </a>
                 </div>
             </div>
         </div>
-        <div class="h-auto flex border-b-4 text-white justify-center">
-
-            <div class="">
-                <div class="">
-                My Company Logo
-                </div>
-            </div>
-            
-            <div class=" ">
-                <div class="">
-                My Company Logo
-                </div>
-            </div>
-        </div>
+        
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <script>
+            AOS.init();
+        </script>
     </body>
 </html>
